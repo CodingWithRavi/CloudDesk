95248700f861c55c4dddff741c7db755ee88dffc
Ravi kumar <134354002+CodingWithRavi@users.noreply.github.com>
2026-09-07 12:55:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+using CloudDesk.cs.Models;
+using CloudDesk.Data;
+using CloudDesk.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CloudDesk.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Identity
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// MVC
+builder.Services.AddControllersWithViews();
+
+// ⭐ SignalR
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// MVC routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+// ⭐ SignalR Hub
+app.MapHub<ChatHub>("/chatHub");
+
+await SeedData.CreateAsync(app.Services);
+
+app.Run();
